6ac3e4bf3988d83630ebd78190f9243c76bf188e
gitiii777 <lifumin2333@icloud.com>
2025-08-31 20:03:02 +0800

    修复我的页面横屏问题

--- app/src/main/java/com/example/githubapp/ui/githubscreen/UserProfileScreen.kt
+++ app/src/main/java/com/example/githubapp/ui/githubscreen/UserProfileScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -64,7 +66,8 @@ fun UserProfileScreen(
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState()),
             contentAlignment = Alignment.Center
         ) {
             if (authManager.isLoggedIn.value) {
